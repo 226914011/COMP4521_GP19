bce6d7c9ce2fe51b8d3c451ce016f392dee089d6
FrankieChan03 <ykchanbn@connect.ust.hk>
2025-05-03 17:14:21 +0800

    Rename home page to main page

--- mahjong_scoring_calculator/lib/main.dart
+++ mahjong_scoring_calculator/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
+import 'pages/main_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
